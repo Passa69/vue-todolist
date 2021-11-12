035124c80c56516841dc5c5e55ba434867515721
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-12 17:26:54 +0100

    tentativo di correzione esercizio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,54 @@
+// var app = new Vue (
+//     {
+//         el: "#myapp",
+//         data: {
+//             newTask: "",
+//             tasks: [
+//                 "fare la spesa",
+//                 "andare in palestra",
+//                 "fare il pieno alla macchina",
+//                 "lavare il cane"
+//             ]
+//         },
+//         methods: {
+//             removeItem (index) {
+//                 this.tasks.splice(index,1);
+//             },
+
+//             addTask () {
+//                 if(this.newTask.length > 0 && this.newTask[0] !== ' ') {
+//                     this.tasks.push(this.newTask);
+//                     this.newTask = "";
+//                 }   
+//             }
+//         }
+//     }
+// );
 var app = new Vue (
     {
         el: "#myapp",
         data: {
             newTask: "",
-            tasks: [
-                "fare la spesa",
-                "andare in palestra",
-                "fare il pieno alla macchina",
-                "lavare il cane"
+            tasks: [ 
+                {
+                    text: "fare la spesa",
+                    done: "true"
+                },
+
+                {
+                    text: "andare in palestra",
+                    done: "false"
+                },
+
+                {
+                    text: "fare il pieno alla macchina",
+                    done: "false"
+                },
+
+                {
+                    text: "lavare il cane",
+                    done: "true"
+                }  
             ]
         },
         methods: {
